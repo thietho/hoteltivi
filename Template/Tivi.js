5a5b751727365729dd9cc78251ff95dc49a54bdf
Hồ Lư <luthietho@gmail.com>
2022-09-10 10:26:13 +0700

    Update

--- Template/Tivi.js
+++ Template/Tivi.js
@@ -5,26 +5,32 @@ function myEventHandler(event) {
     $('#log').html(event.keyCode);
     switch (event.keyCode) {
         case 13:
-
+            Tivi.toggleFullScreen();
+            console.log('Ok')
             break;
         case 38: //Move top
             //$('.slick-prev').click();
             Tivi.moveTop();
+            console.log('Top')
             break;
         case 40: //Move down
             //$('.slick-next').click();
             Tivi.moveDouwn();
             break;
         case 37: //Move left
-
+            document.getElementById("showvideo").exitFullscreen();
             break;
         case 39: //Move right
 
             break;
         case 461: //Back
         case 8: //Back
-            common.showLoading();
-            window.history.back();
+            if (!document.mozFullScreen && !document.webkitFullScreen && !document.fullscreen) {
+                common.showLoading();
+                window.history.back();
+            }else {
+                Tivi.toggleFullScreen();
+            }
             break;
         case 602: //Portal
             common.showLoading();
@@ -53,8 +59,6 @@ $(document).ready(function () {
     }
 
 
-
-
     Tivi.selectCurent();
 });
 var timer;
@@ -62,7 +66,7 @@ Tivi = {
     current: 0,
     max: $('.sidebar-carousel .item').length,
     itemdisplay: 3,
-    itemheigth:170,
+    itemheigth: 170,
     selectCurent: function () {
         $('.sidebar-carousel .item').removeClass('slick-current');
         $($('.sidebar-carousel .item')[this.current]).addClass('slick-current');
@@ -75,23 +79,23 @@ Tivi = {
             if (this.current >= this.itemdisplay - 1) {
                 var numberstep = this.current - (this.itemdisplay - 1);
                 var top = -(this.itemheigth * numberstep)
-                $('.sidebar-carousel').animate({top: top+'px'});
+                $('.sidebar-carousel').animate({top: top + 'px'});
             }
 
         }
     },
     moveDouwn: function () {
-        if (Tivi.current < Tivi.max-1) {
+        if (Tivi.current < Tivi.max - 1) {
             this.current++;
             this.selectCurent();
             if (this.current > this.itemdisplay - 1) {
                 var numberstep = this.current - (this.itemdisplay - 1);
                 var top = -(this.itemheigth * numberstep)
-                $('.sidebar-carousel').animate({top: top+'px'});
+                $('.sidebar-carousel').animate({top: top + 'px'});
             }
         }
     },
-    playVideo:function () {
+    playVideo: function () {
         var sitemapid = $('.video-list .slick-current').attr('sitemapid');
         $('#showvideo').show();
         $('#showvideo').attr('src', $('[sitemapid=' + sitemapid + ']').attr('video'));
@@ -109,5 +113,21 @@ Tivi = {
             });
             $('#showvideo').hide();
         };
+    },
+    videoElement: document.getElementById("showvideo"),
+    toggleFullScreen: function () {
+        if (!document.mozFullScreen && !document.webkitFullScreen && !document.fullscreen) {
+            if (this.videoElement.mozRequestFullScreen) {
+                this.videoElement.mozRequestFullScreen();
+            } else {
+                this.videoElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+            }
+        } else {
+            if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else {
+                document.webkitCancelFullScreen();
+            }
+        }
     }
 }
